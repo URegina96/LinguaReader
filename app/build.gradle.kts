53b4322633334aadb84d35606a7dc7884f6109e3
regina <usmanova.re@yandex.ru>
2024-10-16 09:57:06 +0500

    Build_gradle.kts and libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -50,20 +51,33 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.core.ktx)  // Основная библиотека Android KTX
+    implementation(libs.androidx.lifecycle.runtime.ktx)  // Жизненный цикл KTX
+    implementation(libs.androidx.activity.compose)  // Поддержка компонентов Activity для Compose
+    implementation(platform(libs.androidx.compose.bom))  // BOM для управления версиями Compose
+    implementation(libs.androidx.ui)  // Основной модуль Compose UI
+    implementation(libs.androidx.ui.graphics)  // Библиотека графики Compose
+    implementation(libs.androidx.ui.tooling.preview)  // Поддержка предпросмотра в Android Studio
+    implementation(libs.androidx.material3)  // Material Design 3 для Jetpack Compose
+    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.room.runtime) // Основная библиотека Room
+    kapt(libs.androidx.room.compiler) // Компилятор Room для аннотаций
+    implementation(libs.androidx.room.ktx) // KTX версия Room для удобства использования
+    implementation ("androidx.core:core-ktx:1.13.1")
+
+
+    // Тестирование
+    testImplementation(libs.junit)  // JUnit для юнит-тестирования
+    androidTestImplementation(libs.androidx.junit)  // JUnit для UI тестирования
+    androidTestImplementation(libs.androidx.espresso.core)  // Espresso для UI тестов
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    androidTestImplementation(libs.androidx.ui.test.junit4)  // JUnit 4 для тестирования UI Compose
+
+    // Дебаг зависимости
+    debugImplementation(libs.androidx.ui.tooling)  // Предпросмотр инструментов
+    debugImplementation(libs.androidx.ui.test.manifest)  // Манифест для тестирования
+}
+kapt {
+    correctErrorTypes = true
 }
